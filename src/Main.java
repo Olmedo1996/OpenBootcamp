093aef922188d5b52ecbc565bb637055687380a7
diego <diegoolmedog96@gmail.com>
2022-11-18 15:22:49 -0300

    primera parte del eneunciado del ejercicio - Ejercicios tema 3 - funciones

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //Primera parte
+        int resultado;
+        resultado = sum(4, 2, 5);
+        System.out.println(resultado);
+    }
+
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+}

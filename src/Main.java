78c420bd8371ca2a1afaf9f1b29bf3393fba889e
diego <diegoolmedog96@gmail.com>
2022-11-18 15:24:01 -0300

    segunda parte del eneunciado del ejercicio - Ejercicios tema 3 - funciones

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,32 @@ public class Main {
         int resultado;
         resultado = sum(4, 2, 5);
         System.out.println(resultado);
+
+        //Segunda parte
+        int incremPuer;
+        incremPuer = 1;
+
+        Coche miCoche = new Coche(4);
+
+        incremPuer=miCoche.incrementarPuertas(incremPuer);
+
+        System.out.println("Número de puertas que tiene el objeto (coche): "+incremPuer);
     }
 
     public static int sum(int a, int b, int c) {
         return a + b + c;
     }
+}
+
+class Coche {
+    int puertas;
+
+    public Coche (int p) {
+        puertas=p;
+    }
+
+    public int incrementarPuertas(int incremPuer){
+        return puertas+incremPuer;
+    }
+
 }
